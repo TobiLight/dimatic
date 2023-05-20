5628032f64e3aa24a34ed29a552a47bfc1970dc3
TobiLight <jgetitdonefast@gmail.com>
2023-05-20 18:27:04 +0100

    dfgh

--- src/lib/components/Tesimonial.svelte
+++ src/lib/components/Tesimonial.svelte
@@ -1,145 +1,109 @@
 <div class="testimonial grid sm:grid-cols-2 gap-12">
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
 	<video width="auto" controls>
 		poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-		<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 		<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-		<source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
 		<track kind="captions" src="" srclang="en" label="English" />
 		Your browser does not support the video tag.
 	</video>
