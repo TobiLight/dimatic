8ce3631843f2f3b87d6ad6b795f57cf2dd5e42d0
TobiLight <jgetitdonefast@gmail.com>
2023-05-20 18:32:32 +0100

    jhgfds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,12 +31,7 @@
 		<div class="sect-2 flex items-center justify-center mt-12">
 			<video class="bg-gray-600" width="auto" controls>
 				poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg">
-				<source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" />
 				<source src="https://archive.org/download/ElephantsDream/ed_hd.avi" type="video/avi" />
-				<source
-					src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4"
-					type="video/mp4"
-				/>
 				<track kind="captions" src="" srclang="en" label="English" />
 				Your browser does not support the video tag.
 			</video>
