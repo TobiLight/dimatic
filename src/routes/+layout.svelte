b49c51d006196e5104405084f42b07d1971b8f3c
TobiLight <jgetitdonefast@gmail.com>
2023-05-20 18:29:27 +0100

    fgbhnm

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 
 <svelte:head>
   <title>Digitimatic</title>
+  <meta name="description" content="This is an example of a
+meta description. This will often show up in search results.">
 </svelte:head>
 
 <main class="min-h-screen py-8 px-4 md:w-2/3 md:mx-auto">
